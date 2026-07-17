c46f8058e02aa7cbb0dbc7e5f72dfd4d1604a686
Yuxiao Wang😎 <yuxiaowang@microsoft.com>
2026-07-17 15:06:54 -0700

    polish

--- src/i18n.js
+++ src/i18n.js
@@ -118,7 +118,7 @@ export const translations = {
 
     // 页脚
     copyright: '© Sayaka-4987 2026',
-    poweredBy: '由 React 和 Ant Design 驱动'
+    poweredBy: 'Powered by React and Ant Design'
   },
   en: {
     // 标题和菜单
